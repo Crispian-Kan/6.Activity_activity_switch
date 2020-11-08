6347540913c20e5fc8149f247cb5dc54b1a37fd2
Jim-Kan <tmpss0909889@gmail.com>
2020-11-08 19:59:06 +0800

    activity exchange
    first version
    create document

---
+++ Project_Activity_Exchange 拷貝/app/src/main/java/com/example/project_activity_exchange/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.example.project_activity_exchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+    public void btn_clickToA(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
